174e23ba5902e574bbf8486516d8811312c1389a
root <root@ip-172-31-22-61.ec2.internal>
2020-11-10 20:41:42 +0000

    updated app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">above!</p>
+                <p className="text-center">HADI BAKALIM KOLAY GELSIN</p>
             )}
             {items.map(item => (
                 <ItemDisplay
